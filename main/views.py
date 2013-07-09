94865201701d67a5bbfa3343ab9b108dbef13763
Denis Lee <denislee@gmail.com>
2013-07-09 01:09:19 -0300

    All files.

---
+++ main/views.py
@@ -0,0 +1,42 @@
+import datetime
+
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, HttpResponseRedirect
+from django.utils.timezone import utc
+# from django.contrib.auth.decorators import login_required
+
+# from stories.models import Story
+# from stories.forms import StoryForm
+
+def index(request):
+	if request.method == 'POST':
+		if request.POST.get('q') == '':
+			return HttpResponseRedirect('/')
+
+		# faz a requisicao para o piratebay aqui
+
+	else:
+		return render(request, 'main/index.html')
+
+# @login_required
+# def story(request):
+# 	if request.method == 'POST':
+# 		form = StoryForm(request.POST)
+# 		if form.is_valid():
+# 			story =	form.save(commit=False)
+# 			story.moderator = request.user
+# 			story.save()
+# 			return HttpResponseRedirect('/')
+# 	else:
+# 		form = StoryForm()
+# 	return render(request, 'stories/story.html', {'form': form})
+
+# @login_required
+# def vote(request):
+# 	story = get_object_or_404(Story, pk=request.POST.get('story')) 
+# 	story.points += 1
+# 	story.save()
+# 	user = request.user
+# 	user.liked_stories.add(story)
+# 	user.save()
+# 	return HttpResponse()
---
+++ torrent/models.py
@@ -0,0 +1,18 @@
+from urlparse import urlparse
+
+from django.db import models
+
+class Torrent(models.Model):
+	title = models.CharField(max_length=200)
+	url = models.URLField()
+	seeders = models.IntegerField(default=0)
+	leechers = models.IntegerField(default=0)
+	size = models.CharField(max_length=200)
+	uploaded_at = models.CharField(max_length=200)
+
+	@property
+	def domain(self):
+		return urlparse(self.url).netloc
+
+	def __unicode__(self):
+		return self.title	
