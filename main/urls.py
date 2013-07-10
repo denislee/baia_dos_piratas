28bc55b4b0181744fe2fd4411fb77a181fc959d2
Denis Lee <denislee@gmail.com>
2013-07-10 00:12:20 -0300

    Other bootstrap theme.

--- main/urls.py
+++ main/urls.py
@@ -1,8 +1,5 @@
 from django.conf.urls import patterns, include, url
 
-# from django.contrib import admin
-# admin.autodiscover()
-
 urlpatterns = patterns('',
 	url(r'^about/', 'main.views.about', name='about'),
 	url(r'^legendas/', 'opensubtitles.views.index', name='opensubtitles'),
