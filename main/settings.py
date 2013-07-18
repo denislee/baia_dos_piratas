a635c4512d6a810a3db20fb8deeebf9f00aec25a
Denis Lee <denislee@gmail.com>
2013-07-18 17:04:51 -0300

    Pipeline settings update.

--- main/settings.py
+++ main/settings.py
@@ -71,8 +71,8 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = os.path.join(PROJECT_PATH, 'static')
-# STATIC_ROOT = 'static'
+# STATIC_ROOT = os.path.join(PROJECT_PATH, 'static')
+STATIC_ROOT = 'static'
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
@@ -80,7 +80,7 @@ STATIC_URL = '/static/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
-        os.path.join(PROJECT_PATH, 'static'),
+    os.path.join(PROJECT_PATH, 'static'),
 )
 
 # List of finder classes that know how to find static files in
@@ -96,8 +96,8 @@ PIPELINE = True
 PIPELINE_VERSION = True
 PIPELINE_URL = '/static/'
 
-STATICFILES_STORAGE = 'pipeline.storage.PipelineCachedStorage'
-PIPELINE_STORAGE = 'pipeline.storage.PipelineFinderStorage'
+STATICFILES_STORAGE = 'pipeline.storage.PipelineStorage'
+PIPELINE_STORAGE = 'pipeline.storage.PipelineStorage'
 
 PIPELINE_CSSMIN_BINARY = 'cssmin'
 PIPELINE_CSS_COMPRESSOR = 'pipeline.compressors.cssmin.CSSMinCompressor'
