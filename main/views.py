6e770c4b44966481c451179862a3a20daa582a48
Denis Lee <denislee@gmail.com>
2013-07-22 23:35:09 -0300

    Debug off.

--- main/views.py
+++ main/views.py
@@ -16,29 +16,29 @@ def index(request):
 	if request.method == 'POST' and q != '':
 		query = toQuote( request.POST.get('q') )
 
-		print '--- opensubtitles ---'
-		print 'query: ' + q
+		# print '--- opensubtitles ---'
+		# print 'query: ' + q
 		movie = getFirstMovie(query)
-		print 'selected movie: ' + str(movie['name'])
-		print 'movie id: ' + str(movie['id'])
+		# print 'selected movie: ' + str(movie['name'])
+		# print 'movie id: ' + str(movie['id'])
 
-		print '--- piratebay ---'
-		print 'query: ' + movie['name'] 
+		# print '--- piratebay ---'
+		# print 'query: ' + movie['name'] 
 		torrents = getTorrents(toQuote(movie['name']))
-		print 'torent 1 name: ' + torrents[0][1]
-		print 'torent 2 name: ' + torrents[1][1]
+		# print 'torent 1 name: ' + torrents[0][1]
+		# print 'torent 2 name: ' + torrents[1][1]
 
-		print 'torent 1 numb: ' + str( torrents[0][1].find('Uploaded') )
-		print 'torent 2 numb: ' + str( torrents[1][1].find('Uploaded') )
+		# print 'torent 1 numb: ' + str( torrents[0][1].find('Uploaded') )
+		# print 'torent 2 numb: ' + str( torrents[1][1].find('Uploaded') )
 
-		# checking again
-		if torrents[0][1].find('Uploaded') > 0:
-			newTitle = torrents[0][1]
-			torrents[0][1] = newTitle[:newTitle.find('Uploaded')]
+		# # checking again
+		# if torrents[0][1].find('Uploaded') > 0:
+		# 	newTitle = torrents[0][1]
+		# 	torrents[0][1] = newTitle[:newTitle.find('Uploaded')]
 
-		if torrents[1][1].find('Uploaded') > 0:
-			newTitle = torrents[1][1]
-			torrents[1][1] = newTitle[:newTitle.find('Uploaded')]
+		# if torrents[1][1].find('Uploaded') > 0:
+		# 	newTitle = torrents[1][1]
+		# 	torrents[1][1] = newTitle[:newTitle.find('Uploaded')]
 
 		link = subtitle(torrents[0][1], movie['id'])
 		torrents[0].append(link)
@@ -46,13 +46,13 @@ def index(request):
 		link = subtitle(torrents[1][1], movie['id'])
 		torrents[1].append(link)
 
-		print 'torrent 1 link: ' + str(torrents[0][6])
-		print 'torrent 2 link: ' + str(torrents[1][6])
+		# print 'torrent 1 link: ' + str(torrents[0][6])
+		# print 'torrent 2 link: ' + str(torrents[1][6])
 
-		print 'torrent 1: ' + str(torrents[0])
-		print 'torrent 2: ' + str(torrents[1])
+		# print 'torrent 1: ' + str(torrents[0])
+		# print 'torrent 2: ' + str(torrents[1])
 
-		print '--- opensubtitles ---'
+		# print '--- opensubtitles ---'
 		# request subtitles
 
 		# difflib.get_close_matches('Hello', subtitles)
